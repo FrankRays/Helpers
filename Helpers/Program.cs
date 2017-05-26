964b0e2571814ba78180ab874cc957f0b6b04886
Julio Cesar Abarca Amezcua <juliotec2@hotmail.com>
2017-05-25 20:31:55 -0500

    Changes

--- Helpers/Program.cs
+++ Helpers/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Abarsoft.Helpers;
+using System.Linq;
 
 namespace Helpers
 {
@@ -7,11 +10,15 @@ namespace Helpers
         static void Main(string[] args)
         {
             //Convertidor vs ExpresionSerializar multiple expressions
-            for (var i = 0; i < 5; i++)
-            {
-                Console.WriteLine($@"Convertidor: {UnitTest.Convertidor()} milliseconds");
-                Console.WriteLine($@"ExpressionSerializer: {UnitTest.ExpressionSerializer()} milliseconds");
-            }
+            /* for (var i = 0; i < 5; i++)
+             {
+                 Console.WriteLine($@"Convertidor: {UnitTest.Convertidor()} milliseconds");
+                 Console.WriteLine($@"ExpressionSerializer: {UnitTest.ExpressionSerializer()} milliseconds");
+             }
+             */
+            var c = new ColeccionSet<int>(new List<int> { 1, 2, 3 }).Where(a => a == 1).FirstOrDefault();
+
+            Console.WriteLine($@"must be 1 = {c}");
         }
     }
 }
--- README.md
+++ README.md
@@ -11,6 +11,7 @@ var convertidor = new Convertidor();
 Expression<Func<int>> e = () => 1;
 XElement x = convertidor.ToXElement(e);
 Expression result2 = convertidor.ToExpression(x);
+```
 
 or this one ...
 
@@ -22,4 +23,15 @@ Expression<Func<int, IEnumerable<Order[]>>> e =
 	 select c.Orders.ToArray();
 XElement x = convertidor.ToXElement(e);
 Expression result12 = convertidor.ToExpression(x);
+```
+
+##ColeccionSet<T>
+
+###ColeccionSet<T> : IList<T>, IList, ICollection<T>, ICollection, IEnumerable<T>, IEnumerable, IBindingListView, IBindingList, ICancelAddNew, IRaiseItemChangedEvents, INotifyCollectionChanged, IQueryable<T>, IQueryable, IOrderedQueryable<T>, IOrderedQueryable
+
+All collections in one, you can use it in Windows Forms inside DataGridView, in WPF in your controls, etc.
+
+```
+var c = new ColeccionSet<int>(new List<int> { 1, 2, 3 }).Where(a => a == 1).FirstOrDefault();
+Console.WriteLine($@"must be 1 = {c}");
 ```
